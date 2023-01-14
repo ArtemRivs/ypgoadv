36b344c3b6b5a7595d894b55d7fa0e36f3c29912
Artem Rivs <artem.rivs@gmail.com>
2023-01-14 17:28:00 +0000

    Update l8

--- sprint1/l8/main.go
+++ sprint1/l8/main.go
@@ -7,24 +7,23 @@ import (
 
 func main() {
 
-//     time.AfterFunc(1*time.Second, func() {
-//         fmt.Println("hi from AfterFunc")
-//     })
-//     fmt.Println("hi")
-//     time.Sleep(2 * time.Second)
-//     fmt.Println("goodbye")
-	
-//     start := time.Now()
-//     timer := time.NewTimer(2 * time.Second) // создаём таймер
-//     t := <-timer.C                          // ожидаем срабатывания таймера
-//     fmt.Println(t.Sub(start).Seconds())     // выводим разницу во времени	
+	//     time.AfterFunc(1*time.Second, func() {
+	//         fmt.Println("hi from AfterFunc")
+	//     })
+	//     fmt.Println("hi")
+	//     time.Sleep(2 * time.Second)
+	//     fmt.Println("goodbye")
 
-    start := time.Now()
-    ticker := time.NewTicker(2 * time.Second)
-    for i := 0; i < 10; i++ {
-        t := <-ticker.C
-        fmt.Println(int(t.Sub(start).Seconds()))
-    }
-	
-	
+	// start := time.Now()
+	// timer := time.NewTimer(2 * time.Second) // создаём таймер
+	// t := <-timer.C                          // ожидаем срабатывания таймера
+	// fmt.Println(t.Sub(start).Seconds())     // выводим разницу во времени
+
+	start := time.Now()
+	ticker := time.NewTicker(2 * time.Second)
+	for i := 0; i < 10; i++ {
+		t := <-ticker.C
+		fmt.Println(int(t.Sub(start).Seconds()))
+	}
+
 }
