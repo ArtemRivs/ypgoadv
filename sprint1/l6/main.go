9b63ef07848d5352087ceb218d26415182a58c5d
Artem Rivs <artem.rivs@gmail.com>
2023-01-14 13:52:03 +0000

    Update l6

--- sprint1/l6/main.go
+++ sprint1/l6/main.go
@@ -2,23 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 )
 
 func main() {
-	currentTimeStr := "2021-09-19T15:59:41+03:00"
-	// скопируйте блок себе в IDE и допишите код
-	layout := time.RFC3339
-	currentTime, err := time.Parse(layout, currentTimeStr)
-	if err != nil {
-		log.Fatal(err)
+
+	start := time.Now()
+
+	for i := 0; i < 9999; i++ {
+
 	}
-	fmt.Println(currentTime)
+	stop := time.Now()
 
-	now := time.Now()
+	fmt.Println(stop.Sub(start))
 
-	fmt.Println("Is", now, "before", currentTime, "? Answer:", now.Before(currentTime))
-	fmt.Println("Is", now, "after", currentTime, "? Answer:", now.After(currentTime))
-	fmt.Println("Is", now, "equal", currentTime, "? Answer:", now.Equal(currentTime))
 }
