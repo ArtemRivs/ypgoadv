e91f2e8bb0386421affc10634e3a882634ab72fb
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2023-01-14 22:05:45 +0500

    Update main.go

--- sprint1/l8/main.go
+++ sprint1/l8/main.go
@@ -14,9 +14,17 @@ func main() {
 //     time.Sleep(2 * time.Second)
 //     fmt.Println("goodbye")
 	
-    start := time.Now()
-    timer := time.NewTimer(2 * time.Second) // создаём таймер
-    t := <-timer.C                          // ожидаем срабатывания таймера
-    fmt.Println(t.Sub(start).Seconds())     // выводим разницу во времени	
+//     start := time.Now()
+//     timer := time.NewTimer(2 * time.Second) // создаём таймер
+//     t := <-timer.C                          // ожидаем срабатывания таймера
+//     fmt.Println(t.Sub(start).Seconds())     // выводим разницу во времени	
 
+    start := time.Now()
+    ticker := time.NewTicker(2 * time.Second)
+    for i := 0; i < 10; i++ {
+        t := <-ticker.C
+        fmt.Println(int(t.Sub(start).Seconds()))
+    }
+	
+	
 }
