6ee708ab61bb089f42f8bb850b4c4bf3237a7d94
Artem Rivs <artem.rivs@gmail.com>
2023-01-14 13:05:16 +0000

    Update l4

--- sprint1/l5/main.go
+++ sprint1/l5/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "time"
+	"fmt"
+	"log"
+	"time"
 )
 
 func main() {
-    currentTimeStr := "2021-09-19T15:59:41+03:00"
-    // скопируйте блок себе в IDE и допишите код
-    layout := "..."
-    currentTime, err := time.Parse(layout, currentTimeStr)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(currentTime)
+	currentTimeStr := "2021-09-19T15:59:41+03:00"
+	// скопируйте блок себе в IDE и допишите код
+	layout := time.RFC3339
+	currentTime, err := time.Parse(layout, currentTimeStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(currentTime)
 
-    now := time.Now()
+	now := time.Now()
 
-    fmt.Println("Is", now, "before", currentTime, "? Answer:", now.Before(currentTime))
-    fmt.Println("Is", now, "after", currentTime, "? Answer:", now.After(currentTime))
-    fmt.Println("Is", now, "equal", currentTime, "? Answer:", now.Equal(currentTime))
+	fmt.Println("Is", now, "before", currentTime, "? Answer:", now.Before(currentTime))
+	fmt.Println("Is", now, "after", currentTime, "? Answer:", now.After(currentTime))
+	fmt.Println("Is", now, "equal", currentTime, "? Answer:", now.Equal(currentTime))
 }
