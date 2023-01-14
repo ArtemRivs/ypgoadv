86c939987ebe2ae7b588689f83d962b5cfd06eec
Artem Rivs <artem.rivs@gmail.com>
2023-01-14 11:31:43 +0000

    add l1 code

--- sprint1/l1/main.go
+++ sprint1/l1/main.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	fmt.Println(now)
+	// timeStr := now.Format("1.2.06 3:4:5 -07 MST")
+	timeStr := now.Format("02.01.2006 15:4:5")
+
+	fmt.Println(timeStr)
+}
