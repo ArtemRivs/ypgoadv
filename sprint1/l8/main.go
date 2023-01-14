0cb846f430282f44b44c8a5db77f809fffd6cb30
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2023-01-14 21:58:07 +0500

    Update main.go

--- sprint1/l8/main.go
+++ sprint1/l8/main.go
@@ -7,11 +7,16 @@ import (
 
 func main() {
 
-    time.AfterFunc(1*time.Second, func() {
-        fmt.Println("hi from AfterFunc")
-    })
-    fmt.Println("hi")
-    time.Sleep(2 * time.Second)
-    fmt.Println("goodbye")
+//     time.AfterFunc(1*time.Second, func() {
+//         fmt.Println("hi from AfterFunc")
+//     })
+//     fmt.Println("hi")
+//     time.Sleep(2 * time.Second)
+//     fmt.Println("goodbye")
+	
+    start := time.Now()
+    timer := time.NewTimer(2 * time.Second) // создаём таймер
+    t := <-timer.C                          // ожидаем срабатывания таймера
+    fmt.Println(t.Sub(start).Seconds())     // выводим разницу во времени	
 
 }
