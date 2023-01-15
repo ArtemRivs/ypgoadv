e893ad0d3ec4ae8862da99d48c21d3a31df34b78
Artem Rivs <artem.rivs@gmail.com>
2023-01-15 11:17:24 +0000

    Add l2

--- sprint2/l2/main.go
+++ sprint2/l2/main.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "time"
+	"context"
+	"time"
 )
 
 const (
-    waitDur    = 1 * time.Second
-    cancelDur  = 2000 * time.Millisecond
-    timeoutDur = 500 * time.Millisecond
+	waitDur    = 1 * time.Second
+	cancelDur  = 2000 * time.Millisecond
+	timeoutDur = 500 * time.Millisecond
 )
 
 type Config struct {
-    SelectTimeout time.Duration
+	SelectTimeout time.Duration
 }
 
 type DB struct {
-    cfg Config
+	cfg Config
 }
 
 type User struct {
-    Name string
+	Name string
 }
 
 func (d *DB) SelectUser(ctx context.Context, email string) (User, error) {
-    ctx2, cancel := /* 1 — допишите здесь создание контекста с тайм-аутом */;
-    defer cancel()
+	ctx2, cancel := context.WithTimeout(ctx, timeoutDur)
+	//* 1 — допишите здесь создание контекста с тайм-аутом */;
+	defer cancel()
 
-    timer := time.NewTimer(waitDur)
-    select {
-    case <-timer.C:
-        return User{Name: "Gosha"}, nil
-    case <-ctx2.Done():
-        return User{}, ctx2.Err()
-    }
+	timer := time.NewTimer(waitDur)
+	select {
+	case <-timer.C:
+		return User{Name: "Gosha"}, nil
+	case <-ctx2.Done():
+		return User{}, ctx2.Err()
+	}
 }
 
 type Handler struct {
-    db *DB
+	db *DB
 }
 
 type Request struct {
-    Email string
+	Email string
 }
