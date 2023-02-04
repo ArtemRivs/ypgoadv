822432bfa06b11b75e8cdcdf8cca7c345619373e
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2023-02-04 18:38:35 +0500

    Update main.go

--- sprint4/l3/main.go
+++ sprint4/l3/main.go
@@ -1,38 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+    "bytes"
+    "encoding/gob"
+    "fmt"
 )
 
-// [{"name":"John Doe"}, {"name":"Вася","comp":"Яндекс"}]
-type Data struct {
-	ID      int    `json:"-"`
-	Name    string `json:"name"`
-	Company string `json:"comp,omitempty"`
-}
-
 func main() {
-	foo := []Data{
-		{
-			ID:   10,
-			Name: "John Doe",
-		},
-		{
-			Name:    "Вася",
-			Company: "Яндекс",
-		},
-	}
-	out, err := json.Marshal(foo)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(out)
-	fmt.Println(string(out))
-
-	var v interface{}
-	err := json.Unmarshal([]byte(`[0,10,30]`), &v)
-	fmt.Printf("%T, %[1]v, %v", v, err)
-
+    // data содержит данные в формате gob
+    data := []byte{12, 255, 129, 2, 1, 2, 255, 130, 0, 1, 12,
+        0, 0, 17, 255, 130, 0, 2, 6, 72, 101, 108, 108,
+        111, 44, 5, 119, 111, 114, 108, 100}
+    // напишите код, который декодирует data в массив строк
+    // 1) создайте буфер `bytes.NewBuffer(data)` для передачи в декодер
+    // 2) создайте декодер gob.NewDecoder(buf)
+    // 3) определите `make([]string, 0)` для получения декодированного слайса
+    // 4) декодируйте данные функцией `dec.Decode`
 }
