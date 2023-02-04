ed44b2fb9c225db42325cf5f6dfcb558b78cf7e9
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2023-02-04 14:34:29 +0500

    Update main.go

--- sprint4/l1/main.go
+++ sprint4/l1/main.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
-func Main() {
-	fmt.Println("It's started")
+type Tree struct {
+    Value float32 `json:"value"`
+    Left  *Tree   `json:"left,omitempty"`
+    Right *Tree   `json:"right,omitempty"`
 }
+
+func main() {
+    rawValue := `{ "value": 100.0, "left": { "value": 80.0 } }`
+    value := Tree{}
+    if err := json.Unmarshal([]byte(rawValue), &value); err != nil {
+        panic(err)
+    }
+    fmt.Printf("%+v", value)
+} 
