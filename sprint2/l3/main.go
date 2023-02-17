d42e03e40ce855ec5f18217bd20258cede2fbb15
Artem Rivs <artem.rivs@gmail.com>
2023-02-17 12:38:52 +0000

    add l1 crypto

--- sprint2/l3/main.go
+++ sprint2/l3/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	wordPtr := flag.String("word", "foo", "a string")
-	fmt.Println("word:", wordPtr)
-
+	// wordPtr := flag.String("word", "foo", "a string")
+	// fmt.Println("word:", wordPtr),
 	// flag.Parse()
 	// fmt.Println("word:", wordPtr)
+	args := os.Args
+	fmt.Printf("All args: %v\n", args)
+	command := os.Args[0]
+	fmt.Printf("Command name: %v\n", command)
+	params := os.Args[1:]
+	fmt.Printf("Params: %v\n", params)
+
 }
