782733b1b8262e1079b5c3f36584745ed473c883
Artem Rivs <79457225+ArtemRivs@users.noreply.github.com>
2023-03-12 22:00:22 +0500

    Create main.go

---
+++ sprint4/l6/main.go
@@ -0,0 +1,13 @@
+func main() {
+    var mu sync.Mutex
+    m := make(map[int]int)
+
+    for i := 0; i < 100; i++ {
+        go func(v int) {
+            mu.Lock()
+            m[v] = v
+            mu.Unlock()
+        }(i)
+    }
+    fmt.Println(len(m))
+} 
