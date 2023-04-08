36affcd0673b693f7cdb39e20a27890341744c90
Artem Rivs <artem.rivs@gmail.com>
2023-04-08 13:45:14 +0100

    add l19

--- sprint4/l19/main.go
+++ sprint4/l19/main.go
@@ -1,65 +1,76 @@
 package main
 
 import (
-    "context"
-    "errors"
-    "fmt"
-    "log"
-    "net/http"
-    "sync"
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
 )
 
 func healthCheck(ctx context.Context, url string, errCh chan<- error, wg *sync.WaitGroup) {
-    var defErr error
-    defer func() {
-        if defErr != nil {
-            select {
-            case errCh <- defErr:
-            case <-ctx.Done(): // теперь здесь используем контекст
-                log.Println("aborting", url)
-            }
-        }
-        wg.Done()
-    }()
+	var defErr error
+	defer func() {
+		if defErr != nil {
+			select {
+			case errCh <- defErr:
+			case <-ctx.Done():
+				log.Println("aborting", url)
+			}
+		}
+		wg.Done()
+	}()
 
-    resp, err := http.Get(url)
-    if err != nil {
-        defErr = fmt.Errorf("healthcheck failed: %w", err)
-        return
-    }
-    if resp.StatusCode != http.StatusOK {
-        defErr = errors.New("healthcheck failed: status not ok")
-        return
-    }
+	resp, err := http.Get(url)
+	if err != nil {
+		defErr = fmt.Errorf("healthcheck failed: %w", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		defErr = errors.New("healthcheck failed: status not ok")
+		return
+	}
 }
 
 func main() {
-    wg := &sync.WaitGroup{}
-    errCh := make(chan error)
-    ctx, cancel := context.WithCancel(context.Background())
-    // контекст на замену stopCh
+	wg := &sync.WaitGroup{}
+	errCh := make(chan error)
+	ctx, cancel := context.WithCancel(context.Background())
 
-    hostsToCheck := []string{
-        "https://ya1ndex.ru",
-        "https://eda.yandex.ru",
-        "https://lavka.yan1dex.ru",
-    }
-    for _, hostToCheck := range hostsToCheck {
-        log.Println("checking", hostToCheck)
-        wg.Add(1)
-        go healthCheck(ctx, hostToCheck, errCh, wg)
-    }
+	hostsToCheck := []string{
+		"https://yandex.ru",
+		"https://google.com",
+		"https://bing.com",
+		"https://test000000001.com",
+		"https://test000000002.com",
+		"https://test000000003.com",
+		"https://test000000004.com",
+		"https://test000000005.com",
+		"https://test000000006.com",
+		"https://test000000007.com",
+		"https://test000000008.com",
+		"https://test000000009.com",
+		"https://ya.ru",
+		"https://lamoda.ru",
+	}
+	for _, hostToCheck := range hostsToCheck {
+		log.Println("checking", hostToCheck)
+		wg.Add(1)
+		go healthCheck(ctx, hostToCheck, errCh, wg)
+	}
 
-    go func() {
-        wg.Wait()
-        close(errCh)
-    }()
+	go func() {
+		wg.Wait()
+		close(errCh)
+	}()
 
-    if err := <-errCh; err != nil {
-        log.Println(err)
-        cancel()
-        return
-    }
+	if err := <-errCh; err != nil {
+		log.Println(err)
+		//time.Sleep(2 * time.Second)
+		cancel()
+		return
+	}
 
-    log.Println("successful healthcheck")
+	log.Println("successful healthcheck")
 }
