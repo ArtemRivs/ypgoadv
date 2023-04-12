e463c69adb5ec46535e2772503e50d12f8871b8d
Artem Rivs <artem.rivs@gmail.com>
2023-04-12 19:30:19 +0100

    change main.go

--- sprint4/l22/main.go
+++ sprint4/l22/main.go
@@ -1,64 +1,66 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "log"
-    "net/http"
-    "sync"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
 )
 
 func healthCheck(url string, errCh chan<- error, wg *sync.WaitGroup, stopCh <-chan struct{}) {
-    var defErr error
-    defer func() {
-        if defErr != nil {
-            select {
-            // первая горутина, поймавшая ошибку, сможет записать в канал
-            case errCh <- defErr: 
-            // остальные завершат работу, провалившись в этот case
-            case <-stopCh: 
-                log.Println("aborting", url)
-            }
-        }
-        wg.Done()
-    }()
+	var defErr error
+	defer func() {
+		if defErr != nil {
+			select {
+			// первая горутина, поймавшая ошибку, сможет записать в канал
+			case errCh <- defErr:
+			// остальные завершат работу, провалившись в этот case
+			case <-stopCh:
+				log.Println("aborting", url)
+			}
+		}
+		wg.Done()
+	}()
 
-    resp, err := http.Get(url)
-    if err != nil {
-        defErr = fmt.Errorf("healthcheck failed: %w", err)
-        return
-    }
-    if resp.StatusCode != http.StatusOK {
-        defErr = errors.New("healthcheck failed: status not ok")
-        return
-    }
+	resp, err := http.Get(url)
+	if err != nil {
+		defErr = fmt.Errorf("healthcheck failed: %w", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		defErr = errors.New("healthcheck failed: status not ok")
+		return
+	}
 }
 
 func main() {
-    wg := &sync.WaitGroup{}
-    errCh := make(chan error)
-    stopCh := make(chan struct{})
-    hostsToCheck := []string{
-        "https://google.com",
-        "https://test000000001.com",
-        "https://example.com",
-    }
-    for _, hostToCheck := range hostsToCheck {
-        log.Println("checking", hostToCheck)
-        wg.Add(1)
-        go healthCheck(hostToCheck, errCh, wg, stopCh)
-    }
-    // в отдельной горутине ждём завершения всех healthCheck после этого закрываем канал errCh — больше записей не будет
-    go func() {
-        wg.Wait()
-        close(errCh)
-    }()
+	wg := &sync.WaitGroup{}
+	errCh := make(chan error)
+	stopCh := make(chan struct{})
+	hostsToCheck := []string{
+		"https://google.com",
+		"https://test000000001.com",
+		"https://yahoo.com",
+		"https://example.com",
+	}
+	for _, hostToCheck := range hostsToCheck {
+		log.Println("checking", hostToCheck)
+		wg.Add(1)
+		go healthCheck(hostToCheck, errCh, wg, stopCh)
+	}
+	// в отдельной горутине ждём завершения всех healthCheck после этого закрываем канал errCh — больше записей не будет
+	go func() {
+		wg.Wait()
+		log.Println("wait group!")
+		close(errCh)
+	}()
 
-    if err := <-errCh; err != nil {
-        log.Println(err)
-        close(stopCh)
-        return
-    }
+	if err := <-errCh; err != nil {
+		log.Println(err)
+		close(stopCh)
+		return
+	}
 
-    log.Println("successful healthcheck")
-} 
+	log.Println("successful healthcheck")
+}
